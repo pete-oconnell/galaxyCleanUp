d82e8a9b0fc8afbb784590048a3b931188088841
Pete O'Connell <manchesteroc@gmail.com>
2020-01-30 09:12:17 +0000

    Initial creation of repo with pre-existing code

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # galaxyCleanUp
+This is a basic C# application which will perform a single run through of all objects and should there be any templates which does not have anything derived will be deleted unless the template name exists in the proctectedObjects.txt file
+
+# Usage
+!! Ensure sufficient backups are implemented prior to running this application !!
+
+The application when being executed will request for the galaxy host, name, username & password. If your galaxy does not implement security simply leaving the username and password blank is acceptable.
+The application runs on a single pass of all objects, finding templates without any instances or templates derived from them and will look to delete them if they're not listed in the protected objects file.
+The application will report how many templates it has deleted, when it can no longer delete any unused templates the application will report 0 objects deleted.
+
+Unsupported
+-----------
+
+The primary implications of being UNSUPPORTED are:
+
+1. This code has had LIMITED testing and may not work completely as intended and may have unintended side effects.
+1. This includes NO WARRANTY OF ANY KIND. AVEVA assumes NO responsibility for this code or any unintended consequences of using them.
+1. By using this code, you assume FULL responsibility for the consequences.
+1. The code may fail to work following a product update (patch, service pack, major release) that makes changes to existing database objects.
+1. Wonderware/AVEVA assumes no responsibility to answer questions or assist with the use of the code themselves (although, to the degree they leverage standard product features, those are of course supported).
+1. Requests of support for this code should be raised via an issue on github which it will be investigated in due course
---
+++ galaxyCleanUp/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ArchestrA.GRAccess;
+
+namespace galaxyCleanUp
+{
+    class Program
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        static void Main(string[] args)
+        {
+            string node;
+            string galaxyName;
+            string galaxyUser;
+            string galaxyPass;
+            List<string> protectedTemplates = new List<string>();
+            string line;
+
+
+            Console.Write("Enter Galaxy Node name: ");
+            node = Console.ReadLine();
+            Console.Write("Enter Galaxy Name: ");
+            galaxyName = Console.ReadLine();
+            Console.Write("Enter Galaxy Username: ");
+            galaxyUser = Console.ReadLine();
+            Console.Write("Enter Galaxy Password: ");
+            galaxyPass = Console.ReadLine();
+
+            System.IO.StreamReader file = new System.IO.StreamReader(@"protectedObjects.txt");
+
+            while ((line = file.ReadLine()) != null)
+            {
+                protectedTemplates.Add(line.ToLower());                
+            }
+
+            try
+            {
+                log.Info("Attempting to connect to " + galaxyName + " using a username of " + galaxyUser);
+                GRAccessApp grAccess = new GRAccessAppClass();
+
+                IGalaxies gals = grAccess.QueryGalaxies(node);
+                log.Info("Found " + gals.count + " galaxies");
+                IGalaxy galaxy = gals[galaxyName];
+                log.Info("Attempting to log into galaxy");
+                galaxy.Login(galaxyUser, galaxyPass);
+
+                IgObjects objs = galaxy.QueryObjects(EgObjectIsTemplateOrInstance.gObjectIsTemplate, EConditionType.NameEquals, "thisshouldnevermatch", EMatch.NotMatchCondition);
+
+                log.Info("Found " + objs.count + " objects");
+
+                int x = 0;
+                foreach (ITemplate template in objs)
+                {
+                    IgObjects derivedtemplates = galaxy.QueryObjects(EgObjectIsTemplateOrInstance.gObjectIsTemplate, EConditionType.derivedOrInstantiatedFrom, template.Tagname, EMatch.MatchCondition);
+                    IgObjects derivedinstances = galaxy.QueryObjects(EgObjectIsTemplateOrInstance.gObjectIsInstance, EConditionType.derivedOrInstantiatedFrom, template.Tagname, EMatch.MatchCondition);
+                    log.Info("Checking " + template.Tagname + " for derived templates");
+                    if (derivedtemplates.count == 0)
+                    {
+                        log.Info(template.Tagname + " has no derived templates, checking instances now");
+                        if (derivedinstances.count == 0)
+                        {
+                            if (!protectedTemplates.Contains(template.Tagname.ToLower()))
+                            {
+                                log.Info(template.Tagname + " has no instances, this template will be removed from the galaxy");
+                                x++;
+                                template.DeleteTemplate();
+                            }
+                            else
+                            {
+                                log.Info(template.Tagname + " has no instances but is a protected object so will not be deleted");
+                            }
+                        }
+                    }
+                }
+
+                galaxy.Logout();
+
+                Console.WriteLine("Finished. {0} objects deleted, press return to exit", x.ToString());
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+}
